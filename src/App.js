b31523368a4ca835fdf5348cfc00cab76f4338d0
micheledinelli <dinellimichele00@gmail.com>
2022-06-14 17:50:27 +0200

    error page added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import ErrorPage from "./components/ErrorPage.js";
+
 import Landing from "./components/Landing.js";
 import GamePage from "./components/GamePage.js";
 
-function App() {
-    // return (
-    //     <div className="App">
-    //         <Landing />
-    //     </div>
-    // );
 
+function App() {
+    /**
+     * Switch on route, the lat one matches all the paths and returns error page
+     */
     return (
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Landing />}></Route>
-            <Route path="/gamePage" element={<GamePage />}></Route>
-          </Routes>
+            <Routes>
+                <Route path="/gamePage" element={<GamePage />}></Route>
+                <Route path="/" element={<Landing />}></Route>
+                <Route path="*" element={<ErrorPage />}></Route>
+            </Routes>
         </BrowserRouter>
     );
 }
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,6 +6,7 @@ import GameImage from '../assets/undraw_game_day_ucx9.png';
 import FrontOffice from '../assets/undraw_feeling_proud_qne1.png';
 import BackOffice from '../assets/undraw_learning_sketching_nd4f.png';
 
+import '../scss/style.scss';
 import '../css/landing.css';
 
 const Landing = () => {
@@ -23,14 +24,14 @@ const Landing = () => {
                     <div className="col-sm-4 align-self-center text-section">
                         <p className="display-4 text-muted lh-1">Discover our games!</p>
                         <p className="display-4 text-muted lh-1">Tons of new animals waiting for you!</p>
-                        <button className="mt-3 btn btn-outline-dark">Explore games</button>
+                        <button className="mt-3 btn btn-primary text-white">Explore games</button>
                     </div>
                 </div>
                 <div className="row mt-5">
                     <div className="col-sm-4 align-self-center text-section text-swap-on-bp">
                         <p className="display-4 text-muted lh-1">Get access to our products and services!</p>
                         <p className="display-4 text-muted lh-1">All through the front office.</p>
-                        <button className="mt-3 btn btn-outline-dark">Front office</button>
+                        <button className="mt-3 btn btn-primary text-white">Front Office</button>
                     </div>
                     <div className="col-sm-8">
                         <img className="img-fluid img-swap-on-bp" src={FrontOffice}></img>
@@ -43,7 +44,7 @@ const Landing = () => {
                     <div className="col-sm-4 align-self-center text-section">
                         <p className="display-4 text-muted lh-1">Manage the application thanks to the back office.</p>
                         <p className="display-4 text-muted lh-1">For admins only.</p>
-                        <button className="mt-3 btn btn-outline-dark">Back office</button>
+                        <button className="mt-3 btn btn-primary text-white">Back office</button>
                     </div>
                 </div>
             </div>
