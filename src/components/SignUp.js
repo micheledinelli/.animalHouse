87208d2e83ae3449ef7142be18512d5bb4d3113f
micheledinelli <dinellimichele00@gmail.com>
2022-06-15 19:12:24 +0200

    registration... waiting for backend upload

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../../node_modules/bootstrap-icons/font/bootstrap-icons.css";
@@ -8,32 +10,95 @@ import registerImage from '../assets/undraw_welcome_cats_thqn.png';
 
 const SignUp = () => {
 
+    const navigate = useNavigate();
+
+    const [myerror, setError] = React.useState();
+
+    const [data, setData] = React.useState({
+        name: "",
+        surname: "",
+        email: "",
+        password: ""
+    });
+
+    const handleChange = ({currentTarget: input}) => {
+        setData({...data, [input.name]: input.value});
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const url = "http://localhost:8080/api/users";
+            const {data: res} = await axios.post(url, data);
+            console.log(res.message);
+        } catch (error) {
+            if(error.response && error.response.status >= 400 && error.response.status <= 500) {
+                setError(error.response.data.message);
+            }
+        }
+    }
+
     return(
         <div className="container mt-2 text-center">
             <h1 className="display-4">Sign Up!</h1>
             <div className="row container">
                 <div className="col-lg-6">
                     <img src={registerImage} className="img-fluid" alt="loginImage"></img>
+                    {
+                        myerror && <span className="text-primary">{myerror}</span>
+                    }
                 </div>
                 <div className="col-lg-6 align-self-center p-5">
-                    <form className="text-center">
+                    <form className="text-center" onSubmit={handleSubmit}>
                         <div className="d-flex justify-content-around">
                             <div className="form-floating mb-3">
-                                <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-                                <label htmlFor="floatingInput">Name</label>
+                                <input 
+                                    type="text" 
+                                    className="form-control" 
+                                    name="name" 
+                                    onChange={handleChange}
+                                    value={data.name}
+                                    placeholder="name" 
+                                    required 
+                                />
+                                <label htmlFor="name">Name</label>
                             </div>
                             <div className="form-floating">
-                                <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-                                <label htmlFor="floatingPassword">Surname</label>
+                                <input 
+                                    type="text" 
+                                    className="form-control" 
+                                    name="surname" 
+                                    onChange={handleChange}
+                                    value={data.surname}
+                                    placeholder="surname" 
+                                    required 
+                                />
+                                <label htmlFor="surname">Surname</label>
                             </div>
                         </div>
                         <div className="form-floating mb-3">
-                            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com" />
-                            <label htmlFor="floatingInput">Email address</label>
+                            <input 
+                                type="email" 
+                                className="form-control" 
+                                name="email" 
+                                onChange={handleChange}
+                                value={data.email}
+                                placeholder="name@example.com" 
+                                required 
+                            />
+                            <label htmlFor="email">Email address</label>
                         </div>
                         <div className="form-floating">
-                            <input type="password" className="form-control" id="floatingPassword" placeholder="Password" />
-                            <label htmlFor="floatingPassword">Password</label>
+                            <input 
+                                type="password" 
+                                className="form-control" 
+                                name="password" 
+                                onChange={handleChange}
+                                value={data.password}
+                                placeholder="Password" 
+                                required 
+                            />
+                            <label htmlFor="password">Password</label>
                         </div>
                         <button type="submit" className="btn btn-outline-secondary mt-3">Submit</button>                            
                     </form>
