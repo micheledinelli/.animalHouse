26980a8cc047fd07dba852a6fbcc4482ebfcd345
micheledinelli <dinellimichele00@gmail.com>
2022-06-16 15:32:20 +0200

    scores path

---
+++ src/backend/routes/scores.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const { Score } = require('../models/score');
+
+router.get('/', async(req, res) => {
+    try {
+
+        const scores = await Score.find();
+
+        if(scores) {
+            res.send(scores);
+        } else {
+            res.status(200).send({message: "No scores available"});
+        }
+
+    } catch (error) {
+        res.status(500).send({message: "Internal server error"});
+    }
+})
+
+module.exports = router; 
