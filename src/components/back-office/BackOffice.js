9d39021866d164127736742308ae3eea9aa3a5a3
micheledinelli <dinellimichele00@gmail.com>
2022-06-20 12:26:41 +0200

    landing back office and some css

--- src/components/back-office/BackOffice.js
+++ src/components/back-office/BackOffice.js
@@ -1,10 +1,42 @@
 import React from "react";
 
+import { Link } from 'react-router-dom';
+
+import { Slide, Fade } from "react-awesome-reveal";
+
+import '../../css/back-office.css';
+
+import People from "../../assets/undraw_People_re_8spw.png";
+import Manage from "../../assets/undraw_Social_update_re_xhjr.png";
+
 const BackOffice = () => {
     return(
-        <>
-            hello im back office
-        </>
+        <div className="container"> 
+            <h1 className="display-3 text-center">Welcome to <b>BackOffice</b></h1>
+            <div className="row mt-5 mx-3">
+                <div className="col-lg-6">
+                    <Link to="/">
+                    <img src={People} className="img-fluid zoom"></img>
+                    </Link>
+                    <Slide direction="left" delay={600}>
+                        <p className="display-5 text-center">Personal data</p>
+                    </Slide>
+                </div>
+                <div className="col-lg-6">
+                    <Link to="/">
+                        <img src={Manage} className="img-fluid zoom"></img>
+                    </Link>
+                    <Slide direction="right" delay={600}>
+                        <p className="display-5 text-center">Showcase</p>
+                    </Slide>
+                </div>
+            </div>
+            <div className="container text-center w-50 d-grid gap-2">
+                <Link to="/">
+                    <button className="btn btn-outline-primary btn-lg" type="button">Go back</button>
+                </Link>
+            </div>
+        </div>
     )
 }
 
