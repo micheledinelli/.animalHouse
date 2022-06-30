fdc912fe1c8dba516655bac26c9d54dcd991ba37
micheledinelli <dinellimichele00@gmail.com>
2022-06-30 14:34:19 +0200

    hangman proceeds ok...

--- src/components/game/Hangman.js
+++ src/components/game/Hangman.js
@@ -16,6 +16,9 @@ const Hangman = () => {
 
     const [randomWord, setRandomWord] = useState();
     const userGuess = [];
+    const alphabet = Array.from(Array(26))
+                            .map((e, i) => i + 65)
+                            .map((x) => String.fromCharCode(x));
 
     useEffect(() => {
         getRandomWord();
@@ -26,7 +29,7 @@ const Hangman = () => {
         if(randomWord) {
             console.log(randomWord);
             const rootDiv = document.getElementById("guess-word-div");
-            let vowels = ["a", "e", "i", "o", "u"];
+            let vowels = ["A", "E", "I", "O", "U"];
             for(let i = 0; i < randomWord.length; i++) {
                 let div = document.createElement("div");
                 let input = document.createElement("input");
@@ -35,8 +38,11 @@ const Hangman = () => {
                 div.classList.add("p-2");
                 
                 input.type = "text";
+                input.style.textTransform = "uppercase";
+
                 input.classList.add("form-control");
                 input.classList.add("form-control-lg");
+
                 input.onchange = handleChange;
                 input.name = i;
 
@@ -48,7 +54,8 @@ const Hangman = () => {
                 
                 } else if(randomWord[i] == ' ') {
                    input.disabled = "true";
-                   userGuess[i] = " ";
+                   userGuess[i] = ' ';
+                   p.innerHTML = ' '
                 } else {
                     p.innerHTML = '-';
                 }
@@ -73,7 +80,14 @@ const Hangman = () => {
     }
 
     const handleChange = ({currentTarget: input}) => {
-        userGuess[Number(input.name)] = input.value; 
+        
+        let inputLetter = input.value.toUpperCase();
+        
+        if(alphabet.includes(input.value)) {
+            console.log("ok");
+            userGuess[Number(input.name)] = input.value; 
+            document.getElementById(`span-${input.value}`);
+        }
     }
 
     const handleDifficult = (e) => {
@@ -105,17 +119,35 @@ const Hangman = () => {
                 <button className="btn btn-outline-secondary mx-3" value={1} onClick={handleDifficult}>Medium</button>
                 <button className="btn btn-outline-secondary mx-3" value={2} onClick={handleDifficult}>Hard</button>
             </div>
-            <div className="row container text-center">
-                <div className="col-lg-6 bg-secondary p-5 rounded-circle mt-3">
+            <div className="row mt-5">
+                <div className="col-lg-6 bg-primary rounded-3">
+                    <img id="hangman-state" src={Hangman0} className="img-fluid hangman-img"></img>
+                </div>
+                <div className="col-lg-6 d-flex flex-column align-items-center justify-content-center">
+                    <div id="guess-word-div" className="d-flex align-items-center justify-content-center flex-wrap">
+
+                    </div>
+                    <div id="alphabet" className="d-flex align-items-center flex-wrap justify-content-center mt-3">
+                        {
+                            alphabet.map((e) => (
+                                <span className="fw-bold display-6 mx-2" key={e}>{e}</span>
+                            ))
+                        }
+                    </div>
+                    <div className="container mt-5">
+                        <button className="btn btn-outline-success" onClick={checkWin}>Guess</button>
+                    </div>
+                </div>
+                
+                {/* <div className="col-lg-6 bg-secondary p-5 rounded-circle mt-3">
                     <img id="hangman-state" src={Hangman0} className="img-fluid hangman-img"></img>
                 </div>
                 <div className="col-lg-6 d-flex align-items-center mt-5">
                     <form className="p-5">
-                        <div className="d-flex flex-row mb-3" id="guess-word-div">
+                        <div className="d-flex flex-row flex-wrap mb-3" id="guess-word-div">
                         </div>  
-                        <button className="btn btn-outline-success" onClick={checkWin}>Guess</button>
                     </form>
-                </div>
+                </div> */}
             </div>
         </div>
     )
